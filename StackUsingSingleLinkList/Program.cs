d542f60359f73138a7406d13553cd01b7ea625dd
Roqi SB <roqisb9@gmail.com>
2022-11-28 16:46:23 +0700

    create switch case

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -49,9 +49,29 @@ namespace StackUsingSingleLinkList
             {
                 for(tmp = top; tmp != null; tmp = tmp.next)
                 {
-
+                    Console.WriteLine(tmp.info);
                 }
             }    
+        }
+        static void Main(string[]args)
+        {
+            stack s = new stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
+                string Input = Console.ReadLine();
+                char ch = Convert.ToChar(Input == "" ? "0" : Input);
+                switch (ch)
+                {
+
+                }
+            }
         }
 
     }
