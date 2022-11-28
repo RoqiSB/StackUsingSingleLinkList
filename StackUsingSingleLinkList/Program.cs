8d984f3f89f9292fc40c0b74b77a50325900c584
Roqi SB <roqisb9@gmail.com>
2022-11-28 16:23:29 +0700

    create class node

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i,node n)
+        {
+
+        }
+    }
+}
